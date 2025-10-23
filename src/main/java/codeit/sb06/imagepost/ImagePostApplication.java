880e3cfed23bc9196c8cafb028a3931910c19db5
hagyutae <gted221@gmail.com>
2025-10-23 17:06:20 +0900

    first commit

---
+++ src/main/java/codeit/sb06/imagepost/ImagePostApplication.java
@@ -0,0 +1,13 @@
+package codeit.sb06.imagepost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagePostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImagePostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/codeit/sb06/imagepost/ImagePostApplicationTests.java
@@ -0,0 +1,13 @@
+package codeit.sb06.imagepost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagePostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
